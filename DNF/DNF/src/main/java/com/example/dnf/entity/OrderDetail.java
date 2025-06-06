7e527b2c100427eebb4d6800e4f6b36bb406c7f4
paoze <147594224+yimishengyan@users.noreply.github.com>
2025-06-06 16:57:03 +0800

    Delete DNF directory

--- DNF/DNF/src/main/java/com/example/dnf/entity/OrderDetail.java
+++
@@ -1,9 +0,0 @@
-package com.example.dnf.entity;
-
-public class OrderDetail {
-    Integer objectId;
-    String orderName;
-    String buyerName;
-    String sellerName;
-    Double value;
-}
--- DNF/src/main/java/com/example/dnf/entity/test.java
+++
@@ -1,4 +0,0 @@
-package com.example.dnf.entity;
-
-public class test {
-}
